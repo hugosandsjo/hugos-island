218a5300b379abc8d74a5cd90be70f6bb4322006
hugosandsjo <hugosandsjo@gmail.com>
2023-12-08 10:15:15 +0100

    Folder structure

---
+++ app/autoload.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+session_start();
+
+require __DIR__ . '/functions.php';
--- index.php
+++ index.php
@@ -1,5 +1,25 @@
 <?php
 
+declare(strict_types=1);
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+
+new Client(['base_uri' => 'https://www.yrgopelag.se/centralbank/']);
+
+$response = $client->request('POST', 'https://www.yrgopelag.se/centralbank/', [
+    'form_params' => [
+        'startCode' => 'b9a6417f-4df0-4b5f-8b19-319b67fe8d43'
+    ]
+]);
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+echo '<pre>';
+echo $_ENV['API_KEY'];
+
 ?>
 
 <!DOCTYPE html>
@@ -8,11 +28,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+    <title>Hugos-island</title>
+    <link rel="stylesheet" href="assets/style/style.css">
 
 <body>
-    <h1>Hello</h1>
+    <main>
+        <section class="hero">
+            <h1>Hugos island</h1>
+            <form>
+                <input type="text" name="name" placeholder="Name">
+                <input type="text" name="email" placeholder="Email">
+                <input type="text" name="subject" placeholder="Subject">
+                <textarea name="message" placeholder="Message"></textarea>
+                <button type="submit">Send</button>
+            </form>
+        </section>
+
+    </main>
+
 
 </body>
 
