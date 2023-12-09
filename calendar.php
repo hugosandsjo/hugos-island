69fef3df872384606a34ed6a38c357b1c6ca362f
hugosandsjo <hugosandsjo@gmail.com>
2023-12-09 08:00:33 +0100

    Had some problems with the php-calendar

---
+++ calendar.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use benhall14\phpCalendar\Calendar as Calendar;
+
+$calendar = new Calendar();
+$calendar->display();
--- index.php
+++ index.php
@@ -1,24 +1,32 @@
 <?php
-
+// declare strict types
 declare(strict_types=1);
+//require autoload
 require 'vendor/autoload.php';
 
+
+//guzzle
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\ClientException;
 
+
 new Client(['base_uri' => 'https://www.yrgopelag.se/centralbank/']);
 
-$response = $client->request('POST', 'https://www.yrgopelag.se/centralbank/', [
-    'form_params' => [
-        'startCode' => 'b9a6417f-4df0-4b5f-8b19-319b67fe8d43'
-    ]
-]);
+// $response = $client->request('POST', 'https://www.yrgopelag.se/centralbank/', [
+//     'form_params' => [
+//         'startCode' => 'b9a6417f-4df0-4b5f-8b19-319b67fe8d43'
+//     ]
+// ]);
+
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-echo '<pre>';
-echo $_ENV['API_KEY'];
+// echo '<pre>';
+// echo $_ENV['API_KEY'];
+
+// header('Content-Type: application/json');
+
 
 ?>
 
@@ -30,18 +38,28 @@ echo $_ENV['API_KEY'];
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hugos-island</title>
     <link rel="stylesheet" href="assets/style/style.css">
+</head>
 
 <body>
+
     <main>
         <section class="hero">
             <h1>Hugos island</h1>
             <form>
                 <input type="text" name="name" placeholder="Name">
+                <input type="text" name="surname" placeholder="Surname">
                 <input type="text" name="email" placeholder="Email">
-                <input type="text" name="subject" placeholder="Subject">
-                <textarea name="message" placeholder="Message"></textarea>
+                <!-- <input type="date" id="start" name="stay-start" value="2024-01-01" min="2024-01-01" max="2024-01-31" /> -->
+                <select name="room-type">
+                    <option value="budget">Budget</option>
+                    <option value="standard">Standard</option>
+                    <option value="luxury">Luxury</option>
+                </select>
+                <?php require __DIR__ . '/calendar.php'; ?>
                 <button type="submit">Send</button>
             </form>
+
+
         </section>
 
     </main>
