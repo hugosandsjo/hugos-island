bacd31dda456745a093e3dbab13a7f76ec64d6de
hugosandsjo <hugosandsjo@gmail.com>
2023-12-10 16:19:02 +0100

    Worked with some javascript to add the arrival and departure into the form

--- calendar.php
+++ calendar.php
@@ -4,5 +4,85 @@ require 'vendor/autoload.php';
 
 use benhall14\phpCalendar\Calendar as Calendar;
 
+// Create a new calendar
 $calendar = new Calendar();
-$calendar->display();
+
+// $events = array();
+
+// $events[] = array(
+//     'start' => '2024-01-14',
+//     'end' => '2024-01-18',
+//     'summary' => '',
+//     'mask' => true,
+//     'classes' => ['booked']
+// );
+
+function addBookingsToEvents()
+{
+    // Connect to the database
+    $database = new PDO('sqlite:' . __DIR__ . '/app/database/database.db');
+    $statement = $database->prepare('SELECT arrival, departure FROM booking');
+    $statement->execute();
+    $bookings = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+    // Initialize the events array
+    $events = [];
+
+    // Add each booking to the events array
+    foreach ($bookings as $booking) {
+        $events[] = [
+            'start' => $booking['arrival'],
+            'end' => $booking['departure'],
+            'summary' => '',
+            'mask' => true,
+            'classes' => ['booked']
+        ];
+    }
+
+    return $events;
+}
+
+$dates = [
+    '2024-01-01',
+    '2024-01-02',
+    '2024-01-03',
+    '2024-01-04',
+    '2024-01-05',
+    '2024-01-06',
+    '2024-01-07',
+    '2024-01-08',
+    '2024-01-09',
+    '2024-01-10',
+    '2024-01-11',
+    '2024-01-12',
+    '2024-01-13',
+    '2024-01-14',
+    '2024-01-15',
+    '2024-01-16',
+    '2024-01-17',
+    '2024-01-18',
+    '2024-01-19',
+    '2024-01-20',
+    '2024-01-21',
+    '2024-01-22',
+    '2024-01-23',
+    '2024-01-24',
+    '2024-01-25',
+    '2024-01-26',
+    '2024-01-27',
+    '2024-01-28',
+    '2024-01-29',
+    '2024-01-30',
+    '2024-01-31',
+];
+
+// Use the function
+$events = addBookingsToEvents();
+
+print_r($events);
+
+// Add the events to the calendar
+$calendar->addEvents($events);
+
+// Defines what month to display
+$calendar->display(date('2024-01-01'));
--- index.php
+++ index.php
@@ -29,12 +29,12 @@ $dotenv->load();
 // header('Content-Type: application/json');
 
 
-$database = new PDO('sqlite:' . __DIR__ . '/app/database/database.db');
-$statement = $database->prepare('SELECT * FROM rooms');
-$statement->execute();
-while ($row = $statement->fetchObject()) {
-    echo $row->room_class . "<br>";
-}
+// $database = new PDO('sqlite:' . __DIR__ . '/app/database/database.db');
+// $statement = $database->prepare('SELECT * FROM rooms');
+// $statement->execute();
+// while ($row = $statement->fetchObject()) {
+//     echo $row->room_class . "<br>";
+// }
 
 // Form logic
 if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'])) {
@@ -42,11 +42,12 @@ if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'])) {
     $firstname = htmlspecialchars(str_replace(' ', '', trim($_POST['firstname'])));
     $lastname = htmlspecialchars(str_replace(' ', '', trim($_POST['lastname'])));
     $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    $arrival = $_POST['arrival'];
+    $departure = $_POST['departure'];
 
     if ($email === '') {
         $errors[] = 'The email field is missing.';
     } else {
-
         $database = new PDO('sqlite:' . __DIR__ . '/app/database/database.db');
         $statement = $database->prepare('INSERT INTO guests (firstname, lastname, email) VALUES (:firstname, :lastname, :email)');
         $statement->bindParam(':firstname', $firstname, PDO::PARAM_STR);
@@ -60,7 +61,6 @@ if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'])) {
     }
 }
 
-
 ?>
 
 <!DOCTYPE html>
@@ -71,6 +71,7 @@ if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hugos-island</title>
     <link rel="stylesheet" href="assets/style/style.css">
+    <script src="assets/scripts/script.js" defer></script>
 </head>
 
 <body>
@@ -80,13 +81,16 @@ if (isset($_POST['firstname'], $_POST['lastname'], $_POST['email'])) {
             <h1>Hugos island</h1>
             <form action="index.php" method="post">
                 <input type="text" name="firstname" placeholder="Name">
-                <input type="text" name="lastname" placeholder="Surname">
+                <input type="text" name="lastname" placeholder="Lastname">
                 <input type="text" name="email" placeholder="Email">
                 <select name="room-type">
                     <option value="budget">Budget</option>
                     <option value="standard">Standard</option>
                     <option value="luxury">Luxury</option>
                 </select>
+                <input type="text" id="arrival" name="arrival" placeholder="Arrival">
+                <input type="text" id="departure" name="departure" placeholder="Departure">
+
                 <?php require __DIR__ . '/calendar.php'; ?>
                 <button type="submit">Send</button>
             </form>
