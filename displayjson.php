d41dea5b7cde989371a0ef7c5bf0962ea184cb62
hugosandsjo <hugosandsjo@gmail.com>
2023-12-17 22:21:09 +0100

    styling

--- displayjson.php
+++ displayjson.php
@@ -6,6 +6,6 @@ session_start();
 
 if (isset($_SESSION['response'])) {
 
-                    header('Content-Type: application/json');
-                    echo json_encode($_SESSION['response']);
+    header('Content-Type: application/json');
+    echo json_encode($_SESSION['response']);
 }
--- index.php
+++ index.php
@@ -21,60 +21,59 @@ require 'app/autoload.php';
 ?>
 <?php require __DIR__ . '/header.php'; ?>
 <?php require __DIR__ . '/form.php'; ?>
+
 <nav>
-                    <p>Budget</p>
-                    <p>Standard
-                    </p>
-                    <p>Luxury
-                    </p>
+    <p>Budget</p>
+    <p>Standard
+    </p>
+    <p>Luxury
+    </p>
 </nav>
-<header>
-
-                    <p class="introduction">“We have a history of serving the best food in combination with beautiful rooms for your stay. All in combination with beautiful nature and food”</p>
-</header>
 <main>
-                    <section class="hero">
-                                        <div class="hotelName">
-                                                            <h1>Florida inn</h1>
-                                        </div>
-                    </section>
+    <header>
 
-                    <section class="booking">
+        <p class="introduction">“We have a history of serving the best food in combination with beautiful rooms for your stay. All in combination with beautiful nature and food”</p>
+    </header>
+    <section class="hero">
+        <img src="assets/images/apple.png" class="apple">
+    </section>
+    <section class="hotelName">
+        <h1>Florida inn</h1>
+    </section>
+    <section class="booking">
 
-                                        <?php require __DIR__ . '/calendar.php'; ?>
-                                        <form action="index.php" method="post">
-                                                            <input type="text" name="firstname" placeholder="Name">
-                                                            <input type="text" name="lastname" placeholder="Lastname">
-                                                            <input type="text" name="email" placeholder="Email">
-                                                            <input type="text" id="arrival" name="arrival" placeholder="Arrival">
-                                                            <input type="text" id="departure" name="departure" placeholder="Departure">
-                                                            <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code">
-                                                            <select id="room" name="roomType">
-                                                                                <option value="budget">Budget</option>
-                                                                                <option value="standard">Standard</option>
-                                                                                <option value="luxury">Luxury</option>
+        <?php require __DIR__ . '/calendar.php'; ?>
+        <form action="index.php" method="post">
+            <input type="text" name="firstname" placeholder="Name">
+            <input type="text" name="lastname" placeholder="Lastname">
+            <input type="text" name="email" placeholder="Email" class="emailInput">
+            <input type="text" id="arrival" name="arrival" placeholder="Arrival">
+            <input type="text" id="departure" name="departure" placeholder="Departure">
+            <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code" class="transferCode"><br>
+            <select id="room" name="roomType">
+                <option value="budget">Budget</option>
+                <option value="standard">Standard</option>
+                <option value="luxury">Luxury</option>
+            </select><br>
+            <input type="checkbox" id="peanuts" name="peanuts" value="peanuts">
+            <label for="peanuts">Peanuts 2kr</label><br>
+            <input type="checkbox" id="peanuts" name="peanuts" value="peanuts">
+            <label for="vodka">Vodka 3kr</label><br>
 
-                                                            </select><br>
-                                                            <input type="checkbox" id="peanuts" name="peanuts" value="peanuts">
-                                                            <label for="peanuts">Peanuts 2kr</label><br>
-                                                            <input type="checkbox" id="peanuts" name="peanuts" value="peanuts">
-                                                            <label for="vodka">Vodka 3kr</label><br>
+            <button type="submit">Book</button>
 
-
-                                                            <button type="submit">Book</button>
-
-                                                            <?php if (isset($errors)) {
-                                                                                foreach ($errors as $error) {
-                                                                                                    echo '<br>' . $error;
-                                                                                }
-                                                            } ?>
-
-
-
-                                        </form>
+            <?php if (isset($errors)) {
+                foreach ($errors as $error) {
+                    echo '<br>' . $error;
+                }
+            } ?>
 
 
-                    </section>
+
+        </form>
+
+
+    </section>
 
 </main>
 
