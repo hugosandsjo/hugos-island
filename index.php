f9083af302a4f361de500e8bd84ab46f99d04515
hugosandsjo <hugosandsjo@gmail.com>
2023-12-21 14:47:51 +0100

    push before trying transfercodes

--- index.php
+++ index.php
@@ -21,137 +21,137 @@ require 'app/autoload.php';
 <?php require __DIR__ . '/form.php'; ?>
 
 <nav>
-                    <div class="budget" id="budgetNavItem">Budget 3€</div>
-                    <div class="standard" id="standardNavItem">Standard 5€</div>
-                    <div class="luxury" id="luxuryNavItem">Luxury 10€</div>
+     <div class="budget" id="budgetNavItem">Budget 3€</div>
+     <div class="standard" id="standardNavItem">Standard 5€</div>
+     <div class="luxury" id="luxuryNavItem">Luxury 10€</div>
 </nav>
 
 
 <main>
-                    <header>
-                                        <p class="introduction">“We have a history of serving the best food in combination with beautiful rooms for your stay. All in combination with beautiful nature and food”</p>
-                    </header>
-                    <section class="hero">
-                                        <img src="assets/images/apple.png" class="apple">
-                    </section>
+     <header>
+          <p class="introduction">“We have a history of serving the best food in combination with beautiful rooms for your stay. All in combination with beautiful nature and food”</p>
+     </header>
+     <section class="hero">
+          <img src="assets/images/apple.png" class="apple">
+     </section>
 
-                    <section class="hotelName">
-                                        <h1>Florida inn</h1>
-                    </section>
+     <section class="hotelName">
+          <h1>Florida inn</h1>
+     </section>
 
-                    <section class="booking budget">
+     <section class="booking budget">
 
-                                        <!-- Displaying the budget calendar -->
-                                        <?php $events = addBookingsToEvents('1');
-                                        $calendarBudget->addEvents($events['eventsBudget']);
-                                        $calendarBudget->display(date('2024-01-01')); ?>
+          <!-- Displaying the budget calendar -->
+          <?php $events = addBookingsToEvents('1');
+          $calendarBudget->addEvents($events['eventsBudget']);
+          $calendarBudget->display(date('2024-01-01')); ?>
 
-                                        <form action="index.php" method="post">
-                                                            <input type="text" name="firstname" placeholder="Name">
-                                                            <input type="text" name="lastname" placeholder="Lastname">
-                                                            <input type="text" name="email" placeholder="Email" class="emailInput">
-                                                            <input type="text" id="arrivalBudget" name="arrival" placeholder="Arrival">
-                                                            <input type="text" id="departureBudget" name="departure" placeholder="Departure">
-                                                            <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code" class="transferCode"><br>
-                                                            <select id="room" name="roomType">
-                                                                                <option value="budget">Budget</option>
-                                                            </select>
-                                                            <div class="features">
-                                                                                <input class="checkbox" type="checkbox" id="peanuts" name="features[]" value="1"> <!-- value 1 = peanuts feature -->
-                                                                                <label for="peanuts">Peanuts 2kr</label>
-                                                                                <input class="checkbox" type="checkbox" id="vodka" name="features[]" value="2"> <!-- value 2 = vodka feature -->
-                                                                                <label for="vodka">Vodka 3kr</label>
-                                                                                <input class="checkbox" type="checkbox" id="dinner" name="features[]" value="3"> <!-- value 3 = dinner feature -->
-                                                                                <label for="vodka">Three course dinner 4kr</label>
-                                                            </div>
-                                                            <br><button type="submit">Book</button>
+          <form action="index.php" method="post">
+               <input type="text" name="firstname" placeholder="Name">
+               <input type="text" name="lastname" placeholder="Lastname">
+               <input type="text" name="email" placeholder="Email" class="emailInput">
+               <input type="text" id="arrivalBudget" name="arrival" placeholder="Arrival">
+               <input type="text" id="departureBudget" name="departure" placeholder="Departure">
+               <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code" class="transferCode"><br>
+               <select id="room" name="roomType">
+                    <option value="budget">Budget</option>
+               </select>
+               <div class="features">
+                    <input class="checkbox" type="checkbox" id="peanuts" name="features[]" value="1"> <!-- value 1 = peanuts feature -->
+                    <label for="peanuts">Peanuts 2kr</label>
+                    <input class="checkbox" type="checkbox" id="vodka" name="features[]" value="2"> <!-- value 2 = vodka feature -->
+                    <label for="vodka">Vodka 3kr</label>
+                    <input class="checkbox" type="checkbox" id="dinner" name="features[]" value="3"> <!-- value 3 = dinner feature -->
+                    <label for="vodka">Three course dinner 4kr</label>
+               </div>
+               <br><button type="submit">Book</button>
 
-                                                            <?php if (isset($errors)) {
-                                                                                foreach ($errors as $error) {
-                                                                                                    echo '<br>' . $error;
-                                                                                }
-                                                            } ?>
-                                        </form>
+               <?php if (isset($errors)) {
+                    foreach ($errors as $error) {
+                         echo '<br>' . $error;
+                    }
+               } ?>
+          </form>
 
-                    </section>
+     </section>
 
-                    <section class="booking standard">
+     <section class="booking standard">
 
-                                        <!-- displaying the standard calendar -->
-                                        <?php $events = addBookingsToEvents('2');
-                                        $calendarStandard->addEvents($events['eventsStandard']);
-                                        $calendarStandard->display(date('2024-01-01')); ?>
+          <!-- displaying the standard calendar -->
+          <?php $events = addBookingsToEvents('2');
+          $calendarStandard->addEvents($events['eventsStandard']);
+          $calendarStandard->display(date('2024-01-01')); ?>
 
-                                        <form action="index.php" method="post">
-                                                            <input type="text" name="firstname" placeholder="Name">
-                                                            <input type="text" name="lastname" placeholder="Lastname">
-                                                            <input type="text" name="email" placeholder="Email" class="emailInput">
-                                                            <input type="text" id="arrivalStandard" name="arrival" placeholder="Arrival">
-                                                            <input type="text" id="departureStandard" name="departure" placeholder="Departure">
-                                                            <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code" class="transferCode"><br>
-                                                            <select id="room" name="roomType">
-                                                                                <option value="standard">Standard</option>
-                                                            </select>
-                                                            <div class="features">
-                                                                                <input class="checkbox" type="checkbox" id="peanuts" name="features[]" value="1"> <!-- value 1 = peanuts feature -->
-                                                                                <label for="peanuts">Peanuts 2kr</label><br>
-                                                                                <input class="checkbox" type="checkbox" id="vodka" name="features[]" value="2"> <!-- value 2 = vodka feature -->
-                                                                                <label for="vodka">Vodka 3kr</label><br>
-                                                                                <input class="checkbox" type="checkbox" id="dinner" name="features[]" value="3"> <!-- value 3 = dinner feature -->
-                                                                                <label for="vodka">Three course dinner 4kr</label><br>
-                                                            </div>
-                                                            <br><button type="submit">Book</button>
+          <form action="index.php" method="post">
+               <input type="text" name="firstname" placeholder="Name">
+               <input type="text" name="lastname" placeholder="Lastname">
+               <input type="text" name="email" placeholder="Email" class="emailInput">
+               <input type="text" id="arrivalStandard" name="arrival" placeholder="Arrival">
+               <input type="text" id="departureStandard" name="departure" placeholder="Departure">
+               <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code" class="transferCode"><br>
+               <select id="room" name="roomType">
+                    <option value="standard">Standard</option>
+               </select>
+               <div class="features">
+                    <input class="checkbox" type="checkbox" id="peanuts" name="features[]" value="1"> <!-- value 1 = peanuts feature -->
+                    <label for="peanuts">Peanuts 2kr</label><br>
+                    <input class="checkbox" type="checkbox" id="vodka" name="features[]" value="2"> <!-- value 2 = vodka feature -->
+                    <label for="vodka">Vodka 3kr</label><br>
+                    <input class="checkbox" type="checkbox" id="dinner" name="features[]" value="3"> <!-- value 3 = dinner feature -->
+                    <label for="vodka">Three course dinner 4kr</label><br>
+               </div>
+               <br><button type="submit">Book</button>
 
-                                                            <?php if (isset($errors)) {
-                                                                                foreach ($errors as $error) {
-                                                                                                    echo '<br>' . $error;
-                                                                                }
-                                                            } ?>
-                                        </form>
-                    </section>
+               <?php if (isset($errors)) {
+                    foreach ($errors as $error) {
+                         echo '<br>' . $error;
+                    }
+               } ?>
+          </form>
+     </section>
 
-                    <section class="booking luxury">
+     <section class="booking luxury">
 
-                                        <!-- displaying the luxury calendar -->
-                                        <?php $events = addBookingsToEvents('3');
-                                        $calendarLuxury->addEvents($events['eventsLuxury']);
-                                        $calendarLuxury->display(date('2024-01-01')); ?>
+          <!-- displaying the luxury calendar -->
+          <?php $events = addBookingsToEvents('3');
+          $calendarLuxury->addEvents($events['eventsLuxury']);
+          $calendarLuxury->display(date('2024-01-01')); ?>
 
-                                        <form action="index.php" method="post">
-                                                            <input type="text" name="firstname" placeholder="Name">
-                                                            <input type="text" name="lastname" placeholder="Lastname">
-                                                            <input type="text" name="email" placeholder="Email" class="emailInput">
-                                                            <input type="text" id="arrivalLuxury" name="arrival" placeholder="Arrival">
-                                                            <input type="text" id="departureLuxury" name="departure" placeholder="Departure">
-                                                            <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code" class="transferCode"><br>
-                                                            <select id="room" name="roomType">
-                                                                                <option value="luxury">Luxury</option>
-                                                            </select>
-                                                            <div class="features">
-                                                                                <input class="checkbox" type="checkbox" id="peanuts" name="features[]" value="1"> <!-- value 1 = peanuts feature -->
-                                                                                <label for="peanuts">Peanuts 2kr</label><br>
-                                                                                <input class="checkbox" type="checkbox" id="vodka" name="features[]" value="2"> <!-- value 2 = vodka feature -->
-                                                                                <label for="vodka">Vodka 3kr</label><br>
-                                                                                <input class="checkbox" type="checkbox" id="dinner" name="features[]" value="3"> <!-- value 3 = dinner feature -->
-                                                                                <label for="vodka">Three course dinner 4kr</label><br>
-                                                            </div>
-                                                            <br> <button type="submit">Book</button>
+          <form action="index.php" method="post">
+               <input type="text" name="firstname" placeholder="Name">
+               <input type="text" name="lastname" placeholder="Lastname">
+               <input type="text" name="email" placeholder="Email" class="emailInput">
+               <input type="text" id="arrivalLuxury" name="arrival" placeholder="Arrival">
+               <input type="text" id="departureLuxury" name="departure" placeholder="Departure">
+               <input type="text" id="transferCode" name="transferCode" placeholder="Transfer code" class="transferCode"><br>
+               <select id="room" name="roomType">
+                    <option value="luxury">Luxury</option>
+               </select>
+               <div class="features">
+                    <input class="checkbox" type="checkbox" id="peanuts" name="features[]" value="1"> <!-- value 1 = peanuts feature -->
+                    <label for="peanuts">Peanuts 2kr</label><br>
+                    <input class="checkbox" type="checkbox" id="vodka" name="features[]" value="2"> <!-- value 2 = vodka feature -->
+                    <label for="vodka">Vodka 3kr</label><br>
+                    <input class="checkbox" type="checkbox" id="dinner" name="features[]" value="3"> <!-- value 3 = dinner feature -->
+                    <label for="vodka">Three course dinner 4kr</label><br>
+               </div>
+               <br> <button type="submit">Book</button>
 
-                                                            <?php if (isset($errors)) {
-                                                                                foreach ($errors as $error) {
-                                                                                                    echo '<br>' . $error;
-                                                                                }
-                                                            } ?>
-                                        </form>
-                    </section>
-                    <section class="succesfullBooking">
-                                        <?php
+               <?php if (isset($errors)) {
+                    foreach ($errors as $error) {
+                         echo '<br>' . $error;
+                    }
+               } ?>
+          </form>
+     </section>
+     <section class="succesfullBooking">
+          <?php
 
-                                        if (isset($message)) {
-                                                            echo $message;
-                                        }
-                                        ?>
-                    </section>
+          if (isset($message)) {
+               echo $message;
+          }
+          ?>
+     </section>
 
 </main>
 
