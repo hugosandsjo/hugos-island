021f8460a639990405a1d46d521c5f13a8dc9eae
hugosandsjo <hugosandsjo@gmail.com>
2024-01-10 07:54:23 +0100

    keep working on booking.php

--- booking.php
+++ booking.php
@@ -1,17 +1,18 @@
 <?php
 
+declare(strict_types=1);
+
 require __DIR__ . '/header.php';
 
-?>
-<?php
+
+
 
 
 session_start();
 echo "Your total cost is: " . $_SESSION['totalCost'];
 
-?>
 
 
-<?php
+
+
 require __DIR__ . '/footer.php';
-?>
